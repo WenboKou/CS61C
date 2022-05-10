a288df836ddb6da02ff206eb06835465b0cfb645
WenboKou <kouwenbocn@163.com>
2022-05-10 09:58:11 +0800

    done with lab1

--- labs/lab01/ll_cycle.c
+++ labs/lab01/ll_cycle.c
@@ -3,5 +3,23 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise = head;
+    node *hare = head;
+    while (1) {
+        if (hare != NULL && hare->next != NULL) {
+            hare = hare->next;
+            if (hare->next != NULL) {
+                hare = hare->next;
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+        tortoise = tortoise->next;
+        if (hare == tortoise) {
+            return 1;
+        }
+    }
     return 0;
 }
